1391c8496a8e8d2b625849902fb5434f409bc00c
SreenivasNandi07 <sreenivasnandi0706@gmail.com>
2026-05-14 23:39:12 +0530

    nlp based

---
+++ NLP/main.py
@@ -0,0 +1,28 @@
+
+from sklearn.linear_model import LogisticRegression
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+
+texts = [
+    "I love this",
+    "This is amazing",
+    "I hate this",
+    "Worst experience"
+]
+
+labels = [1, 1, 0, 0]
+
+# Convert text into vectors
+vectorizer = TfidfVectorizer()
+X = vectorizer.fit_transform(texts)
+
+# Train model
+model = LogisticRegression()
+model.fit(X, labels)
+
+# Predict
+test = vectorizer.transform(["I really love this product"])
+prediction = model.predict(test)
+
+print(prediction)
